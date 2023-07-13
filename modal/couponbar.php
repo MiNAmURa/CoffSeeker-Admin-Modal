a50cc817db9d79f317422db703cc23c6ad44975a
Edison <kayinortin@hotmail.com>
2023-07-13 14:24:07 +0800

    coupon_bar_edit

--- modal/couponbar.php
+++ modal/couponbar.php
@@ -1,7 +1,6 @@
 <li class="nav-item">
     <!-- data-target 要對應 下面的id   -->
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#Couponbar"
-        aria-expanded="true" aria-controls="collapseTwo">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#Couponbar" aria-expanded="true" aria-controls="collapseTwo">
         <!-- fontawesome -->
         <i class="fa-solid fa-ticket"></i>
         <span>Coupon</span>
@@ -11,7 +10,6 @@
         <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Coupon Option</h6>
             <a class="collapse-item" href="coupon-list.php">Coupon-List</a>
-            <a class="collapse-item" href="coupon.php">Coupon-Detail</a>
             <a class="collapse-item" href="coupon-create.php">Create-Coupon</a>
             <a class="collapse-item" href="coupon-search.php">Coupon-Search</a>
             <a class="collapse-item" href="coupon-edit-list.php">Coupon-Edit</a>
