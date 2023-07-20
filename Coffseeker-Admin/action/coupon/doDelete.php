c4a4f07b82431d26c12ac3c6282daaf658c88ad7
Edison <kayinortin@hotmail.com>
2023-07-20 15:00:12 +0800

    頁面連結變更

--- Coffseeker-Admin/action/coupon/doDelete.php
+++ Coffseeker-Admin/action/coupon/doDelete.php
@@ -12,7 +12,7 @@ $description = "已刪除";
 $sql = "UPDATE coupon SET coupon_valid = $valid, valid_description = '$description' WHERE coupon_id = $id";
 
 if ($conn->query($sql) === TRUE) {
-    header("location: ../../coupon-list.php");
+    header("location: ../../coupon-edit-list.php");
 } else {
     echo "更新資料錯誤: " . $conn->error;
 }
