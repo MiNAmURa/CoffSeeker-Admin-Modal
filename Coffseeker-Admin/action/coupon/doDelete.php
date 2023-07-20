9a9dee14c6bad97ff5b92ac5413744118cf54afe
Edison <kayinortin@hotmail.com>
2023-07-20 14:36:54 +0800

    coupon_delete

--- Coffseeker-Admin/action/coupon/doDelete.php
+++ Coffseeker-Admin/action/coupon/doDelete.php
@@ -2,14 +2,17 @@
 if (!isset($_GET["coupon_id"])) {
     die("無法作業");
 }
+
 require_once("../../../db_connect.php");
 
 $id = $_GET["coupon_id"];
+$valid = 0;
+$description = "已刪除";
 
-$sql = "UPDATE coupon SET coupon_valid = 0 , valid_description='已刪除' WHERE coupon_id = $id";
+$sql = "UPDATE coupon SET coupon_valid = $valid, valid_description = '$description' WHERE coupon_id = $id";
 
 if ($conn->query($sql) === TRUE) {
     header("location: ../../coupon-list.php");
 } else {
-    echo "刪除資料錯誤: " . $conn->error;
+    echo "更新資料錯誤: " . $conn->error;
 }
